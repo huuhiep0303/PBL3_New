854537222d0d5a8c4c22ed7c9d7d5288d36439ab
namtaooo <namp1622@gmail.com>
2025-06-07 17:05:04 +0700

    olodo

--- ShoppingSysten/Entity class/Inventory.cs
+++ ShoppingSysten/Entity class/Inventory.cs
@@ -9,17 +9,18 @@ namespace Entity_class
 		public int inventoryId { get; set; }
 		public int Quantity { get; set; }
 		public decimal ReorderLevel { get; set; }
-		public string Location { get; set; }
+		public DateTime ExpriryDate { get; set; }
 		public DateTime lastUpdate { get; set; }
         public Inventory()
         {
             lastUpdate = DateTime.Now;
         }
-        public Inventory(int ProductId, int initialQuantity, int ReorderQuantity) 
+        public Inventory(int ProductId, int initialQuantity, int ReorderQuantity, DateTime date) 
 		{
 			productId = ProductId;
 			Quantity = initialQuantity;
 			ReorderLevel = ReorderQuantity;
+			ExpriryDate = date;
 			lastUpdate = DateTime.Now;
 		}
 		//Khi hàng dưới 50 thì cần nhập hàng lại
